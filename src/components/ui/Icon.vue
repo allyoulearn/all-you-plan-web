a63954c30218608c467f2a15b789d88f6dd56e8f
Lucas Otero <lucas@Lucass-MacBook-Pro.local>
2026-05-21 19:56:56 -0300

    feat: Icon primitive over heroicons

---
+++ src/components/ui/Icon.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { computed } from 'vue'
+import { outlineIcons, solidIcons } from './iconMap.js'
+
+const props = defineProps({
+  name: { type: String, required: true },
+  size: { type: Number, default: 20 },
+  solid: { type: Boolean, default: false },
+})
+
+const component = computed(
+  () => (props.solid ? solidIcons : outlineIcons)[props.name] || null,
+)
+</script>
+
+<template>
+  <component
+    :is="component"
+    v-if="component"
+    :style="{ width: `${size}px`, height: `${size}px` }"
+    aria-hidden="true"
+  />
+</template>
---
+++ src/components/ui/iconMap.js
@@ -0,0 +1,37 @@
+import * as Outline from '@heroicons/vue/24/outline'
+import * as Solid from '@heroicons/vue/24/solid'
+
+const names = {
+  today: 'HomeIcon',
+  chores: 'ArrowPathIcon',
+  projects: 'Squares2X2Icon',
+  wren: 'SparklesIcon',
+  more: 'EllipsisHorizontalIcon',
+  calendar: 'CalendarIcon',
+  stats: 'ChartBarIcon',
+  journal: 'BookOpenIcon',
+  inbox: 'InboxIcon',
+  chat: 'ChatBubbleLeftRightIcon',
+  review: 'ClipboardDocumentCheckIcon',
+  settings: 'Cog6ToothIcon',
+  search: 'MagnifyingGlassIcon',
+  plus: 'PlusIcon',
+  check: 'CheckIcon',
+  'chevron-left': 'ChevronLeftIcon',
+  'chevron-right': 'ChevronRightIcon',
+  'chevron-down': 'ChevronDownIcon',
+  moon: 'MoonIcon',
+  sun: 'SunIcon',
+  flag: 'FlagIcon',
+  mic: 'MicrophoneIcon',
+  bolt: 'BoltIcon',
+  'arrow-right': 'ArrowRightIcon',
+  filter: 'FunnelIcon',
+}
+
+export const outlineIcons = Object.fromEntries(
+  Object.entries(names).map(([k, v]) => [k, Outline[v]]),
+)
+export const solidIcons = Object.fromEntries(
+  Object.entries(names).map(([k, v]) => [k, Solid[v]]),
+)
